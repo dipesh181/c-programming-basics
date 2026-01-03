9f2652abf56cabb01590a3bc8b333ca2feead79e
Dipesh Prajapati <dipeshprajapat018@gmail.com>
2026-01-03 11:33:34 +0530

    Create 18-2d-array-display.c

---
+++ 18-2d-array-display.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+
+int main()
+{
+    int a[2][2], i, j;
+
+    printf("Enter elements of 2x2 matrix:\n");
+
+    for(i = 0; i < 2; i++)
+    {
+        for(j = 0; j < 2; j++)
+        {
+            scanf("%d", &a[i][j]);
+        }
+    }
+
+    printf("Matrix is:\n");
+
+    for(i = 0; i < 2; i++)
+    {
+        for(j = 0; j < 2; j++)
+        {
+            printf("%d ", a[i][j]);
+        }
+        printf("\n");
+    }
+
+    return 0;
+}
