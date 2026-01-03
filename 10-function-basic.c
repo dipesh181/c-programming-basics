78ac620602ead0bb733aa1b27ebf15507d788577
Dipesh Prajapati <dipeshprajapat018@gmail.com>
2026-01-03 11:27:46 +0530

    Create 10-function-basic.c

---
+++ 10-function-basic.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+// Function definition
+void display()
+{
+    printf("Hello, this is a function");
+}
+
+int main()
+{
+    // Function call
+    display();
+
+    return 0;
+}
