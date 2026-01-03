51fe3f3693068b5fa911227c47d66140b867993f
Dipesh Prajapati <dipeshprajapat018@gmail.com>
2026-01-03 11:20:31 +0530

    Create 02-structure.c

---
+++ 02-structure.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+/*
+Program Name: Structure of C Program
+Author: Dipesh Prajapati
+*/
+
+int main()
+{
+    // Declaration section
+    int a = 10;
+    int b = 20;
+    int sum;
+
+    // Processing section
+    sum = a + b;
+
+    // Output section
+    printf("Sum = %d", sum);
+
+    return 0;
+}
