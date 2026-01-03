6916a04144627e604877c0fca848fcaa20d7c12b
Dipesh Prajapati <dipeshprajapat018@gmail.com>
2026-01-03 11:30:38 +0530

    Create 14-call-by-reference.c

---
+++ 14-call-by-reference.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+void change(int *x)
+{
+    *x = 50;
+    printf("Value inside function = %d\n", *x);
+}
+
+int main()
+{
+    int a = 10;
+
+    printf("Value before function call = %d\n", a);
+
+    change(&a);   // call by reference
+
+    printf("Value after function call = %d\n", a);
+
+    return 0;
+}
